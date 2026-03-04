e605879742e12fd7e9156f17ccf0135f147f79dd
genau777 <genau.f4ck@gmail.com>
2026-03-04 22:32:12 +0300

    adding server files

---
+++ dishserver.cpp
@@ -0,0 +1,61 @@
+#include "dishserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+#include <QString>
+
+DishServer::~DishServer() {
+    mTcpServer->close();
+}
+
+DishServer::DishServer(QObject *parent) : QObject(parent){
+    mTcpServer = new QTcpServer(this);
+    connect(mTcpServer, &QTcpServer::newConnection, this, &DishServer::slotNewConnection);
+    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+        qDebug() << "Server is not started";
+    } else {
+        qDebug() << "Server is started on port 33333";
+    }
+}
+
+void DishServer::slotNewConnection(){
+    mTcpSocket = mTcpServer->nextPendingConnection();
+    mTcpSocket->write("Hello, I am Dish Chooser Server!\r\n");
+    connect(mTcpSocket, &QTcpSocket::readyRead, this, &DishServer::slotServerRead);
+    connect(mTcpSocket, &QTcpSocket::disconnected, this, &DishServer::slotClientDisconnected);
+}
+
+void DishServer::slotServerRead(){
+    QTcpSocket *clientSocket = qobject_cast<QTcpSocket*>(sender());
+    if (!clientSocket) return;
+
+    while (clientSocket -> bytesAvailable() > 0) {
+        QByteArray data = clientSocket->readAll();
+        QString request = QString::fromUtf8(data).trimmed();
+        QString ingredients = parseRequest(request);
+        QString result = findDish(ingredients);
+
+        clientSocket->write(result.toUtf8() + "\n");
+    }
+}
+
+QString DishServer::parseRequest(QString data) {
+    return data.toLower().trimmed();
+}
+
+QString DishServer::findDish(QString ingredients) {
+    if (ingredients.contains("egg") && ingredients.contains("milk"))
+		return "Found: Omelette";
+
+    if (ingredients.contains("картофель"))
+		return "Found: Fried Potatoes";
+
+    return "No dishes found :(";
+}
+
+void DishServer::slotClientDisconnected(){
+    QTcpSocket *clientSocket = qobject_cast<QTcpSocket*>(sender());
+    if(clientSocket) {
+        clientSocket->close();:wq
+        clientSocket->deleteLater();
+    }
+}
---
+++ dishserver.h
@@ -0,0 +1,26 @@
+#ifndef DISHSERVER_H
+#define DISHSERVER_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QtNetwork>
+#include <QByteArray>
+#include <QDebug>
+
+class DishServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DishServer(QObject *parent = nullptr);
+    ~DishServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+    void slotServerRead();
+private:
+    QString parseRequest(QString data);
+    QString findDish(QString ingredients);
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+};
+#endif // DISHSERVER_H
